3710523e3f2292ef4884cf1059a4af747ec18d4c
ValeriaDvornikova <valeri2001.02@gmail.com>
2024-04-21 10:09:49 +0300

    parametrized browser

--- src/test/java/ru/ibs/RunnerTest.java
+++ src/test/java/ru/ibs/RunnerTest.java
@@ -10,7 +10,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @Suite
 @IncludeEngines("cucumber")
 @ConfigurationParameters({
-        @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@IBS"),
+        // @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@IBS"),
         @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/scenarios"),
         @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "ru.ibs.glue"),
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty"),
